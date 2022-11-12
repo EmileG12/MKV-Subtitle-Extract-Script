c23eb840d56c83258055d538cc71f9a1da698181
EmileG12 <aimilios.gaston@gmail.com>
2022-11-12 18:48:24 +0000

    Finished script, uses pattern matching to retrieve track ID and type from mkvmerge information then extracts subtitles from all mkv files in the directory. Works with SubRip and SubStationAlpha formats

--- massextractmkv.sh
+++ massextractmkv.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-file_name=$1
-mkvmerge -i "$file_name"
-
 echo "Testing below"
-tracks=$(LANG=en_US.utf8 LANGUAGE=en_US.utf8 mkvmerge -i "$file_name" | tail --lines=+2)
 IFS='
 '
-
-for track in $tracks; do
-    #echo $track
-    if [[ "$track" =~ Track\ ID\ ([0-9]+).*subtitles\ \(([^\)]+)\) ]]; then 
-    echo ${BASH_REMATCH[1]} ${BASH_REMATCH[2]}
-    fi
+for f in *.mkv; do 
+    echo $f
+    tracks=$(LANG=en_US.utf8 LANGUAGE=en_US.utf8 mkvmerge -i "$f" | tail --lines=+2)
+    for track in $tracks; do
+        echo $track
+        if [[ "$track" =~ Track\ ID\ ([0-9]+).*subtitles\ \(([^\)]+)\) ]]; then 
+        track_ID=${BASH_REMATCH[1]}
+        track_type=${BASH_REMATCH[2]}
+        case $track_type in 
+        SubStationAlpha) ext=".ass";;
+        SubRip) ext=".srt";;
+        esac
+        mkvextract tracks "$f" $track_ID:"${f%.*}$ext"
+        fi
+    done
 done
